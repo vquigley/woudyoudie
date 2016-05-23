2bd46905800202e804b04e50badcaea7356e8b70
vquigley <vinny_182@hotmail.com>
2016-05-23 11:18:13 +0100

    removing protocal

--- static/scripts/predict.js
+++ static/scripts/predict.js
@@ -3,7 +3,7 @@ var debug = false;
 function predict() {
   //Pclass, Name, Sex, Age, SibSp, Parch, Fare, Embarked
   //Ports {'Q': 1, 'C': 0, 'S': 2}
-  $.getJSON('/predict?Pclass=3&Name=Braund, Mr. Owen Harris&Sex=0&Age=22&SibSp=1&Parch=0&Fare=7.25&Embarked=2', function(data) {
+  $.getJSON('predict?Pclass=3&Name=Braund, Mr. Owen Harris&Sex=0&Age=22&SibSp=1&Parch=0&Fare=7.25&Embarked=2', function(data) {
     
     console.log(data);
     var result = data.result;    
